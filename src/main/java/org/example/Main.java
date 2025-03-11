7297a18bd6018ea0f68b93e2b08a1b166538a5a2
furkan-uzmez <furkan.n41@hotmail.com>
2025-03-11 17:14:48 +0300

    çalışıyor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.example.Data.Data;
-import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
@@ -9,22 +7,9 @@ import java.io.IOException;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) throws IOException {
-        Data data = new Data();
-        JSONObject jsonData = data.get_data();
-
-        /*Durak durak = new Durak();
-        durak.setDurak_veri(jsonData.getJSONArray("duraklar"));*/
+        //Data data = new Data();
+        //JSONObject jsonData = data.get_data();
 
         SpringApplication.run(Main.class, args);
     }
-
-    /*@Bean
-    public static Data data() throws IOException {
-        System.out.println("1");
-        return new Data(); // Burada kendi nesneni oluşturabilirsin
-    }
-
-    public static Data getData() {
-        return Main.data;
-    }*/
 }
--- src/main/java/org/example/WebController.java
+++ src/main/java/org/example/WebController.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.example.Data.Data;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
